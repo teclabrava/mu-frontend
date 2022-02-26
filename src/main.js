577b683749bea1cfd85110ebd0b4c2dcd7be2b98
Antonio Cordova <cordovantonio@gmail.com>
2022-02-26 00:25:44 -0500

    Add Bootstrap and header theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/site.css'
+import './assets/css/manu-font.css'
 
 createApp(App).mount('#app')
