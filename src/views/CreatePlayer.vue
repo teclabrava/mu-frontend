040fb378a96cc69d50560b40bec5e6a3fc50c749
Antonio Cordova <cordovantonio@gmail.com>
2022-03-08 22:10:55 -0500

    Fix CreatePlayer.vue

--- src/views/CreatePlayer.vue
+++ src/views/CreatePlayer.vue
@@ -1,43 +1,35 @@
 <template>
-  <div class="container-fluid g-lg-0">
-    <div class="row g-0 pb-5">
-      <div class="col-lg-4 bg-fame d-none d-lg-block"></div>
-      <div class="col-lg-8 offset-lg-4">
-        <div class="site-navbar-small text-center">
-          <div class="p-4">
-            <div class="col-12 col-xl-8 offset-xl-2 pt-5">
-              <h1 class="title text-primary">Nuevo jugador</h1>
-              <form
-                @submit.prevent="onSubmit"
-                class="player-form mt-5">
-                <div class="mb-3">
-                  <label for="nickname" class="form-label text-primary">Nickname</label>
-                  <input v-model="player.nickname" type="text" class="form-control" id="nickname" aria-describedby="nicknameHelp">
-                  <div id="nicknameHelp" class="form-text">Ingresa nombre de jugador</div>
-                </div>
-                <div class="mb-3">
-                  <label for="status" class="form-label text-primary">Estado</label>
-                  <select v-model="player.status" class="form-select" id="status" aria-describedby="statusHelp">
-                    <option value="">Seleccione uno</option>
-                    <option v-for="s in statusArray" :key="s">{{ s }}</option>
-                  </select>
-                  <div id="statusHelp" class="form-text">Selecciona el estado entre las opciones</div>
-                </div>
-                <div class="mb-3">
-                  <label for="avatar" class="form-label text-primary">Avatar</label>
-                  <input type="file" @change="setAvatar" class="form-control" id="avatar" aria-describedby="avatarHelp">
-                  <div id="avatarHelp" class="form-text">Selecciona una imagen para usarlo como tu avatar</div>
-                </div>
-                <div class="mb-3">
-                  <label for="ranking" class="form-label text-primary">Ranking</label>
-                  <input v-model="player.ranking" type="number" class="form-control" id="ranking" aria-describedby="rankingHelp">
-                  <div id="rankingHelp" class="form-text">Ingresa los puntos del jugador</div>
-                </div>
-                <button type="submit" class="btn btn-primary btn-sm me-2" @click="createPlayer">Crear</button>
-                <router-link to="/salon-fama" class="btn btn-secondary btn-sm">Cancelar</router-link>
-              </form>
+  <div class="page-register layout-full pt-5 bd-gray-200">
+    <div class="page align-middle text-center">
+      <div class="page-content ">
+        <div class="p-4 pt-5">
+          <h1 class="title text-primary pt-5">Nuevo jugador</h1>
+          <form
+            @submit.prevent="onSubmit"
+            class="player-form mt-5">
+            <div class="form-floating mb-3">
+              <input v-model="player.nickname" type="text" class="form-control" id="nickname" aria-describedby="nicknameHelp">
+              <label for="nickname">Nickname</label>
             </div>
-          </div>
+            <div class="form-floating mb-3">
+              <select v-model="player.status" class="form-select" id="status" aria-describedby="statusHelp">
+                <option selected>Seleccione uno</option>
+                <option v-for="s in statusArray" :key="s">{{ s }}</option>
+              </select>
+              <label for="status">Selecciona el estado entre las opciones</label>
+            </div>
+
+            <div class="mb-3">
+              <input type="file" @change="setAvatar" class="form-control" id="avatar" aria-describedby="avatarHelp">
+              <div id="avatarHelp" class="form-text">Selecciona una imagen para usarlo como tu avatar</div>
+            </div>
+            <div class="form-floating mb-3">
+              <input v-model="player.ranking" type="number" class="form-control" id="ranking" aria-describedby="rankingHelp">
+              <label for="ranking">Ranking</label>
+            </div>
+            <button type="submit" class="btn bg-primary text-light btn-sm me-2" @click="createPlayer">Crear</button>
+            <router-link to="/salon-fama" class="btn btn-secondary btn-sm">Cancelar</router-link>
+          </form>
         </div>
       </div>
     </div>
